01c16b54590f6f0ccaf4f9fb7901ec5ec16f1fa6
valentina98 <valentina.lilova@abv.bg>
2016-02-23 07:59:02 +0200

    hw

---
+++ lesson11/homework/Valentina Lilova/index.php
@@ -0,0 +1,64 @@
+<?php
+	
+   $fruits = Array(
+   
+   		$bananas=> Array(
+			"price" => 0.50
+			"quantity" =>20,
+			"freshness" => Array(
+				'received' =>'18.02.16',
+				'expirationDate'=>'23.02.16',
+				'eatable'=>'yes',
+			),
+		),
+		
+		$coconuts=> Array(
+			"price" => 1.50
+			"quantity" =>7,
+			"freshness" => Array(
+				'received' =>'15.02.16',
+				'expirationDate'=>'25.02.16',
+				'eatable'=>'yes',
+			),
+		),
+  
+		$oranges=> Array(
+			"price" => 0.35
+			"quantity" =>30,
+			"freshness" => Array(
+				'received' =>'11.02.16',
+				'expirationDate'=>'17.02.16',
+				'eatable'=>'no',
+			),
+		),
+		
+		$apples=> Array(
+			"price" => 00.40
+			"quantity" =>25,
+			"freshness" => Array(
+				'received' =>'12.02.16',
+				'expirationDate'=>'22.02.16',
+				'eatable'=>'yes',
+			),
+		)
+		
+		$mangos=> Array(
+			"price" => 2
+			"quantity" =>25,
+			"freshness" => Array(
+				'received' =>'16.02.16',
+				'expirationDate'=>'22.02.16',
+				'eatable'=>'yes',
+			),
+		)
+   );
+  
+   foreach($fruits as $type=>$value)
+   {
+   		if ($value['freshness']['eatable']=='ýes') {
+			echo 'The '.$type[].' are '.$value['price'].' $ and are eatable until '.$value['freshness']['expirationDate']."<br>";
+		}
+   } 		
+   
+	print_r($fruits);
+?>
