eef5f846fdc5e8a1b617be583703684fb92c9106
valentina98 <valentina.lilova@abv.bg>
2016-02-24 23:51:01 +0200

    fixed, i think

--- lesson11/homework/Valentina Lilova/index.php
+++ lesson11/homework/Valentina Lilova/index.php
@@ -2,63 +2,64 @@
 	
    $fruits = Array(
    
-   		$bananas=> Array(
-			"price" => 0.50
+   		'bananas'=> Array(
+			"price" => 0.50,
 			"quantity" =>20,
 			"freshness" => Array(
 				'received' =>'18.02.16',
 				'expirationDate'=>'23.02.16',
-				'eatable'=>'yes',
-			),
+				'eatable'=>'yes'
+			)
 		),
 		
-		$coconuts=> Array(
-			"price" => 1.50
+		'coconuts'=> Array(
+			"price" => 1.50,
 			"quantity" =>7,
 			"freshness" => Array(
 				'received' =>'15.02.16',
 				'expirationDate'=>'25.02.16',
-				'eatable'=>'yes',
-			),
+				'eatable'=>'yes'
+			)
 		),
   
-		$oranges=> Array(
-			"price" => 0.35
+		'oranges'=> Array(
+			"price" => 0.35,
 			"quantity" =>30,
 			"freshness" => Array(
 				'received' =>'11.02.16',
 				'expirationDate'=>'17.02.16',
-				'eatable'=>'no',
-			),
+				'eatable'=>'no'
+			)
 		),
 		
-		$apples=> Array(
-			"price" => 00.40
+		'apples'=> Array(
+			"price" => 00.40,
 			"quantity" =>25,
 			"freshness" => Array(
 				'received' =>'12.02.16',
 				'expirationDate'=>'22.02.16',
-				'eatable'=>'yes',
-			),
-		)
+				'eatable'=>'yes'
+			)
+		),
 		
-		$mangos=> Array(
-			"price" => 2
+		'mangos'=> Array(
+			"price" => 2,
 			"quantity" =>25,
 			"freshness" => Array(
 				'received' =>'16.02.16',
 				'expirationDate'=>'22.02.16',
-				'eatable'=>'yes',
-			),
+				'eatable'=>'yes'
+			)
 		)
    );
   
-   foreach($fruits as $type=>$value)
+   foreach($fruits as $key=>$value)
    {
-   		if ($value['freshness']['eatable']=='ýes') {
-			echo 'The '.$type[].' are '.$value['price'].' $ and are eatable until '.$value['freshness']['expirationDate']."<br>";
+   		if ($value['freshness']['eatable']=='yes') {
+			echo 'The '.$key.' are '.$value['price'].' $ and are eatable until '.$value['freshness']['expirationDate']."<br>";
 		}
-   } 		
+   };
    
+	echo "<pre>";
 	print_r($fruits);
 ?>
