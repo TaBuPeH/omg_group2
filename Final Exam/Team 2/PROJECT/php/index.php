789a44c23ed01d1aeb219f63892af59f817ff6ca
valentina98 <valentina.lilova@abv.bg>
2016-03-16 21:14:41 +0200

    .

--- Final Exam/Team 2/PROJECT/php/index.php
+++ Final Exam/Team 2/PROJECT/php/index.php
@@ -15,13 +15,14 @@ if(!isset($_SESSION['logged_user']))
 
     <head>
 	
+		<title>Heroes</title>
+		
         <meta charset="utf-8" />
 		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-        <title>Heroes</title>
-
         <link rel="stylesheet" href="../css/clouds.css" />
+        <link rel="stylesheet" href="../css/main.css" />
 
         <script src="../js/jquery.min.js"></script>
         <script src="../js/clouds.js"></script>
@@ -33,34 +34,39 @@ if(!isset($_SESSION['logged_user']))
 	
 	<header>
 		
-		<div class="topLine">
-		
-		
-			<div class="links">
+		<div id="topLine" >
+
+			<nav class="mainContainer">
+				<a href="#">Profile</a>
+				<a href="heroes.php">Create Hero</a>
+				<a href="login.php">Login</a>
+			</nav>
 			
-				<nav>
-					<a href="#">Profile</a>
-					<a href="heroes.php">Create Hero</a>
-					<a href="login.php">Login</a>
-				</nav>
+		</div>
+		
+	</header>
+	
+		<div class="content">
+		
+			<div id="background">
+			
+				<div id="far-clouds" class="stage clouds"></div>
+				<div id="near-clouds" class="stage clouds"></div>
+				<div id="ground" class="stage"></div>
 				
 			</div>
 			
-		</div>
-	
-        <div id="far-clouds" class="stage clouds"></div>
-        <div id="near-clouds" class="stage clouds"></div>
-		<div id="ground" class="stage"></div>
-		
-
-        <div class="battle">
 			
-			<?php
-			//echo  "WELCOME TO OUR WEBSITE, ".$_SESSION['logged_user']['username'];
-			?>
-			
-			<div class="hero"></div>
-			<div class="hero"></div>
+			<div class="battle mainContainer">
+				
+				<?php
+				//echo  "WELCOME TO OUR WEBSITE, ".$_SESSION['logged_user']['username'];
+				?>
+				
+				<div class="heroes"></div>
+				<div class="heroes"></div>
+				
+			</div>
 			
         </div>
 		
