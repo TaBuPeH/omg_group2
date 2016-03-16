a264cdbd80fea3f8a79faa43ebfa1b67efa43da6
valentina98 <valentina.lilova@abv.bg>
2016-03-16 15:44:14 +0200

    .

--- Final Exam/Team 2/PROJECT/php/index.php
+++ Final Exam/Team 2/PROJECT/php/index.php
@@ -1,40 +1,70 @@
 <?php 
 
-
 require_once "db.class.php";
-
-
 
 
 if(!isset($_SESSION['logged_user']))
 {
-	header("Location: login.php");
+	//header("Location: login.php");
 }
 
 ?>
+
+<!doctype html>
 <html>
 
-<head>
-<style>
-body {
-    background-color: white;
-}
-
-h1 {
-    color: maroon;
-    text-align:center;
+    <head>
 	
-} 
-</style>
-</head>
-<body>
-<?php
-echo  "WELCOME TO OUR WEBSITE, ".$_SESSION['logged_user']['username'];
-?>
-<br>
-<a href="logout.php">Logout</a>
+        <meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
+        <title>Heroes</title>
 
-</body>
+        <link rel="stylesheet" href="../css/clouds.css" />
 
+        <script src="../js/jquery.min.js"></script>
+        <script src="../js/clouds.js"></script>
+        <script src="../js/app.js"></script>
+		
+    </head>
+	
+    <body>
+	
+	<header>
+		
+		<div class='topLine'>
+			<div class='mainContainer'>
+			
+				<nav>
+					<a href='#'>Profile</a>
+					<a href='#'>Create</a>
+					<a href='#'>Hero</a>
+					<a href='#'>Login</a>
+				</nav>
+				
+			</div>
+		</div>
+	
+        <div id="far-clouds" class="stage clouds"></div>
+        <div id="near-clouds" class="stage clouds"></div>
+		<div id="ground" class="stage"></div>
+		
+
+        <div class="content">
+			
+			<?php
+			echo  "WELCOME TO OUR WEBSITE, ".$_SESSION['logged_user']['username'];
+			?>
+			
+			<div class="hero"></div>
+			<div class="hero"></div>
+			
+        </div>
+		
+    </body>
+	
 </html>
+
+
+
