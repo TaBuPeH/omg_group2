c7eb5a810f4f1d5f5f9dee3f5a0f7f1374cdb182
ZdravkoNedelchev <zdravo98@gmail.com>
2016-03-16 14:12:35 +0200

    d

---
+++ Final Exam/Team 2/PROJECT/php/heroes.php
@@ -0,0 +1,84 @@
+<html>
+	<head>
+		<link href="main.css" rel='stylesheet'/>
+	</head>
+	<style>
+	
+h1
+{
+	display: block;
+	width: 30%;
+	float: left;
+	margin-left: 35%;
+	text-align: center;
+}
+form
+{
+	display: block;
+	width: 30%;
+	float: left;
+	margin-left: 35%;
+}
+label
+{
+	display: block;
+	width: 100%;
+	float: left;
+	margin: 10px 0px;
+}
+label input,
+label select
+{
+	display: block;
+	width: 70%;
+	float: left;
+	border-radius: 2px;
+	font-size: 18px;
+	border: 1px solid #0000FF;
+}
+label span
+{
+	display: block;
+	width: 30%;
+	float: left;
+}
+#regButton{
+	width:100%;
+	
+}
+	</style>
+	<body>
+		
+		<h1> Create your HERO</h1>
+		
+		<form method="post" action="heroes_db.php">
+			
+			<label>
+				<span>Name:</span>
+				<input type="text" name="name" >
+			</label>
+			
+			<label>
+				<span>STR: </span>
+				<input type="text" name="Strenght" >
+			</label>
+			
+			<label>
+				<span>DEX:</span>
+				<input type="text" name="Dexterity" >
+			</label>
+			
+			<label>
+				<span>VIT: </span>
+				<input type="text" name="Vitality" >
+			</label>
+			<label>
+				<span>INT: </span>
+				<input type="text" name="Intellect" >
+			</label>
+			
+				<input id="regButton" type="submit" value="Create HERO">
+		</form>
+	</body>
+	
+</html>
