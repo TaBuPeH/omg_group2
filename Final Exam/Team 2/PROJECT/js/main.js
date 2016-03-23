07773eca66d11fb8f7a447f85d3d000c7471a943
valentina98 <valentina.lilova@abv.bg>
2016-03-23 16:51:33 +0200

    .

--- Final Exam/Team 2/PROJECT/js/main.js
+++ Final Exam/Team 2/PROJECT/js/main.js
@@ -1,13 +1,13 @@
 document.addEventListener( 'DOMContentLoaded', function () {
 	
-	/*
+	
 	document.getElementById("logButton").addEventListener("click", function(
 	{
 		this.style.opacity = 0.5;
 		document.getElementById("loginForm").style.visibility = "visible";
 	});
 	
-	
+	/*
 	*/
 	
 	
